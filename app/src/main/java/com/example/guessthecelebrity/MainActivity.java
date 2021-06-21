0a0daa2eaf6b129922287ba9f309a69be7d9061a
DevStar999 <wolverinerage99@gmail.com>
2021-06-22 02:26:52 +0530

    Completed implementation to start quiz and move to empty QuizActivity

--- app/src/main/java/com/example/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/guessthecelebrity/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.guessthecelebrity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView infoContentTextView;
+
+    private void initialise() {
+        infoContentTextView = findViewById(R.id.infoContentTextView);
+
+        String textOfContentTextView =  "Guess as many celebrities as you\n" +
+                "can by choosing the answer for\n"+
+                "the celebrity image among it's four\n" +
+                "options in a time of 30 seconds,\n" +
+                "for all the images, to score\n" +
+                "as high as possible. All the Best!";
+        infoContentTextView.setText(textOfContentTextView);
+    }
+
+    public void startQuiz(View view) {
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
     }
 }
