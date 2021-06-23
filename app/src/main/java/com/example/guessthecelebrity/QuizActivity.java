5439c6e498d094442a0cd9026fcb9e2b31f759e7
DevStar999 <wolverinerage99@gmail.com>
2021-06-23 19:33:29 +0530

    Completed the implementation of QuizActivity

--- app/src/main/java/com/example/guessthecelebrity/QuizActivity.java
+++ app/src/main/java/com/example/guessthecelebrity/QuizActivity.java
@@ -2,17 +2,139 @@ package com.example.guessthecelebrity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.CountDownTimer;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+    // Views
+    private TextView timerTextView;
+    private TextView scoreTextView;
+    private ImageView celebImageView;
+    private TextView guessVerdictTextView;
+    private LinearLayout optionLinearLayout;
+
+    // Variables
+    private long defaultGameDuration;
+    private QuizInfo quizInfo;
     private List<CelebInfo> celebsInfo;
+    private Intent intent;
 
     private void initialise() {
+        timerTextView = findViewById(R.id.timerTextView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        celebImageView = findViewById(R.id.celebImageView);
+        guessVerdictTextView = findViewById(R.id.guessVerdictTextView);
+        optionLinearLayout = findViewById(R.id.optionLinearLayout);
+
+        defaultGameDuration = 30*1000;
+        quizInfo = new QuizInfo();
         celebsInfo = (ArrayList<CelebInfo>) getIntent().getExtras().getSerializable("celebsInfo");
+        intent = new Intent(QuizActivity.this, PostQuizActivity.class);
+    }
+
+    private void setGameText() {
+        // Refresh QuizInfo
+        quizInfo.resetAndGenerateData(celebsInfo);
+
+        // Setting Score Text
+        scoreTextView.setText(quizInfo.getCorrectGuesses().toString() + "/" +
+                quizInfo.getTotalQuestionsPlayed().toString());
+
+        // Setting Image for ImageView
+        try {
+            Bitmap celebImageBitmap = new ImageDownloader().execute(quizInfo.getCelebImageSrc()).get();
+            celebImageView.setImageBitmap(celebImageBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            setGameText();
+        }
+
+        // Setting optionButtons' text
+        for (Integer optionNumber=0; optionNumber<4; optionNumber++) {
+            Integer optionResourceId = this.getResources().getIdentifier("optionButton" +
+                    optionNumber.toString(), "id", this.getPackageName());
+            Button optionButton = findViewById(optionResourceId);
+            optionButton.setText(quizInfo.getOptions().get(optionNumber));
+        }
+    }
+
+    private void playGame() {
+        // Set the game text for the first time
+        setGameText();
+
+        // Setting up the post quiz timer as follows
+        final CountDownTimer postQuizTimer = new CountDownTimer(1500, 1500) {
+            @Override
+            public void onTick(long millisUntilFinished) {}
+            @Override
+            public void onFinish() {
+                // Moving onto PostQuizActivity
+                startActivity(intent);
+            }
+        };
+
+        // Start quiz timer
+        final CountDownTimer quizTimer = new CountDownTimer(defaultGameDuration, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText(quizInfo.getQuizInfoHelper()
+                        .convertMilliToTimerDisplay(millisUntilFinished));
+            }
+            @Override
+            public void onFinish() {
+                guessVerdictTextView.setText("Game Over");
+                celebImageView.setImageResource(R.drawable.time_over);
+                optionLinearLayout.setVisibility(View.GONE);
+
+                // Setting up Intent to move to PostQuizActivity
+                intent.putExtra("celebsInfo", (Serializable) celebsInfo);
+                intent.putExtra("totalQuestionsPlayed", quizInfo.getTotalQuestionsPlayed());
+                intent.putExtra("correctGuesses", quizInfo.getCorrectGuesses());
+
+                // Trigger the postQuizTimer as follows -
+                postQuizTimer.start();
+            }
+        };
+        quizTimer.start();
+    }
+
+    public void clickOption(View view) {
+        // Setting Verdict on TextView
+        String guessVerdictTextViewText = guessVerdictTextView.getText().toString();
+        if (quizInfo.checkAnswer(((TextView) view).getText().toString())) {
+            guessVerdictTextViewText += "<font color=#4CBB17>Correct :)</font>";
+        } else {
+            guessVerdictTextViewText += "<font color=#EE4B2B>Wrong :( </font>";
+            guessVerdictTextViewText += "<font color=#000000>"
+                    + "It is " + quizInfo.getCorrectCelebName() + "</font>";
+        }
+        guessVerdictTextView.setText(Html.fromHtml(guessVerdictTextViewText));
+
+        // Setting the timer to show verdict for a limited time i.e. 0.75 seconds and move on to
+        // the next question
+        new CountDownTimer(750,  750) {
+            @Override
+            public void onTick(long millisUntilFinished) { }
+            @Override
+            public void onFinish() {
+                guessVerdictTextView.setText("Guess Verdict : ");
+                guessVerdictTextView.setTextColor(Color.BLACK);
+                setGameText();
+            }
+        }.start();
     }
 
     @Override
@@ -22,10 +144,6 @@ public class QuizActivity extends AppCompatActivity {
 
         initialise();
 
-        Log.i("Info", "The contents for celebsInfo are as follows -");
-        for (CelebInfo currentCelebInfo: celebsInfo) {
-            Log.i("Info", "currentCelebInfo: Name = " + currentCelebInfo.getCelebName() +
-                    ", ImageSrc = " + currentCelebInfo.getCelebImageSrc());
-        }
+        playGame();
     }
 }
