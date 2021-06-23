bbb7e2b18ee8dd2eb961100372579bafd69c0ee8
DevStar999 <wolverinerage99@gmail.com>
2021-06-23 15:34:26 +0530

    Checkpoint 1

--- app/src/main/java/com/example/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/guessthecelebrity/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.guessthecelebrity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private TextView infoContentTextView;
+    private Button startButton;
+    private DownloadTask task;
+    private List<Pair<String, String>> celebsInfo;
 
     private void initialise() {
         infoContentTextView = findViewById(R.id.infoContentTextView);
+        startButton = findViewById(R.id.startButton);
 
         String textOfContentTextView =  "Guess as many celebrities as you\n" +
                 "can by choosing the answer for\n"+
@@ -22,9 +35,29 @@ public class MainActivity extends AppCompatActivity {
         infoContentTextView.setText(textOfContentTextView);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void startQuiz(View view) {
-        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
-        startActivity(intent);
+        if (!celebsInfo.isEmpty()) {
+            Log.i("Info", "celebsInfo is being passed on to next activity");
+            startButton.setTooltipText("");
+
+            //Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+            //startActivity(intent);
+        }
+    }
+
+    public List<Pair<String, String>> giveAllMatchesOfGroup(String regexPattern, String text,
+                                                            Integer groupNumber1, Integer groupNumber2) {
+        List<Pair<String, String>> ans = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            ans.add(new Pair<> (matcher.group(groupNumber1), matcher.group(groupNumber2)));
+        }
+
+        return ans;
     }
 
     @Override
@@ -33,5 +66,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initialise();
+
+        String celebrityWebsiteHtml = "";
+        while (celebrityWebsiteHtml.isEmpty()) {
+            try {
+                task = new DownloadTask();
+                celebrityWebsiteHtml = task.execute("https://www.imdb.com/list/ls052283250/").get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String regexPattern = "<img alt=\"(.*?)\"(.*?)src=\"(.*?)\"(.*?)>";
+        celebsInfo = giveAllMatchesOfGroup(regexPattern, celebrityWebsiteHtml, 1, 3);
     }
 }
